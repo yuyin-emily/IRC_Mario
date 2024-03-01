e42f8da0210fdf880ebf8228764798dce6b29002
yuyin-emily <emilychen20030716@gmail.com>
2024-03-01 18:52:28 +0800

    bricks

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import pygame
 import datetime
 
+import bricks
+
 pygame.init()
 pygame.display.set_caption("Mario")
 screen = pygame.display.set_mode((640,360))
 clock = pygame.time.Clock()
 clock.tick(55)
-background = pygame.image.load("background.png")
+background = pygame.image.load("./image/background.png")
 
 running = True
+
+brick = []
+for i in range(24): #24塊地板
+        brick.append(bricks.bricks(i))
 
 while running:
     time = datetime.datetime.now().second
@@ -16,10 +22,14 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     
-    print(time)
+    #print(time)
     
     for i in range(5):
         screen.blit(background,[i * 160,0])
+        
+    for i in range(24):
+        screen.blit(brick[i].img,brick[i].position)
+    
 
     pygame.display.update()
 
